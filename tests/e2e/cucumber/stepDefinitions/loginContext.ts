6da469af26eb5ca429850254339f91d6839840b7
Prarup Gurung <grgprarup@gmail.com>
2022-01-28 15:13:39 +0545

    Added implementation for login feature

---
+++ tests/e2e/cucumber/stepDefinitions/loginContext.ts
@@ -0,0 +1,13 @@
+import { Given, Then, When } from "@cucumber/cucumber";
+
+Given('the user has browsed to the login page', function () {
+    
+  });
+
+  When('the use logs in with username {string} and password {string} using the webUI', function (string, string2) {
+   
+  });
+
+  Then('the user should be in homepage', function () {
+   
+  });
